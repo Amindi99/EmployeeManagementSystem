0fd0d612bd47b5d69c74f3877313b79a48f5da26
Amindi99 <amindiperera1@gmail.com>
2021-07-05 00:53:18 +0530

    current work upload

---
+++ src/main/MyConnection.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import java.sql.*;
+/**
+ *
+ * @author Amindi
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/employee_mng","root","");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
